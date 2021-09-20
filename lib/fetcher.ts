2bd73d1ce17e4d3d153546fae7bf06b6958d6c69
Tommy <tommy@tommyjepsen.com>
2021-09-20 11:31:03 +0200

    Updated to latest nextjs, added .env.example and updated postcss with removing jit

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -2,6 +2,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 // export const URL = "http://localhost:3000/api/";
 export const URL = "https://nextjs-typescript-tailwind-api-chart-example.vercel.app/api/";
+// export const URL = process.env.NEXT_PUBLIC_API_URL;
 export const URL_CHART = "v1/chart";
 
 export default fetcher;
