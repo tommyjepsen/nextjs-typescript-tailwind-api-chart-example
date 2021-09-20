7d4c67a009e7c7baea6d9d81a5384d3926714c18
Tommy <tommy@tommyjepsen.com>
2021-09-20 11:11:28 +0200

    Fetcher to prod

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,6 +1,7 @@
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export const URL = "http://localhost:3000/api/"; //https://nextjs-typescript-tailwind-api-chart-example.vercel.app/api/v1/chart
+// export const URL = "http://localhost:3000/api/";
+export const URL = "https://nextjs-typescript-tailwind-api-chart-example.vercel.app/api/";
 export const URL_CHART = "v1/chart";
 
 export default fetcher;
